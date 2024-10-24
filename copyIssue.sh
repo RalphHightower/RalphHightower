099239243aee7b44b2e6805bc19f06534b3397d2
Ralph Hightower <32745442+RalphHightower@users.noreply.github.com>
2024-10-23 21:34:27 -0400

    [fix](build): Update copyIssue.sh

--- copyIssue.sh
+++ copyIssue.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-for i in $(gh issue list -s all | awk '{print $1}'); do gh issue transfer $i RH; echo "$i transferred"; done
+for i in $(gh issue list -s all | awk '{print $1}'); do gh issue transfer $i RalphHightower/RH; echo "$i transferred"; done
